2f55105b920e9212bb0f20d9ab4b405915ce35ac
dlagnlwo <gnlwo7606@naver.com>
2023-09-04 20:57:05 +0900

    [#12] 뷰 페이지 구현

--- src/main/java/com/melon/controller/artist/ArtistController.java
+++ src/main/java/com/melon/controller/artist/ArtistController.java
@@ -1,5 +1,26 @@
 package com.melon.controller.artist;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/artist")
 public class ArtistController {
+
+    @GetMapping
+    public String artist_song(){
+        return "/artist/artist_song";
+    }
+
+    @GetMapping("/album")
+    public String artist_album(){
+        return "/artist/artist_album";
+    }
+
+    @GetMapping("/detail")
+    public String artist_detail(){
+        return "/artist/artist_detail";
+    }
 
 }
