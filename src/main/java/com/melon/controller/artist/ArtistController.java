ab56f0850fc89911213ff031e3a5572acfc43a03
dlagnlwo <gnlwo7606@naver.com>
2023-09-05 10:04:13 +0900

    앨범, 가수 페이지

--- src/main/java/com/melon/controller/artist/ArtistController.java
+++ src/main/java/com/melon/controller/artist/ArtistController.java
@@ -8,16 +8,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/artist")
 public class ArtistController {
 
+    /**
+     * 가수 곡 조회
+     * @author 임휘재
+     */
     @GetMapping
     public String artist_song(){
         return "/artist/artist_song";
     }
 
+    /**
+     * 가수 앨범 조회
+     * @author 임휘재
+     */
     @GetMapping("/album")
     public String artist_album(){
         return "/artist/artist_album";
     }
 
+    /**
+     * 가수 상세 조회
+     * @author 임휘재
+     */
     @GetMapping("/detail")
     public String artist_detail(){
         return "/artist/artist_detail";
