d57a644b133f75d7488f9d80a6a1a668e9a943bd
Kimhoongyu <khun019@naver.com>
2023-09-05 18:55:50 +0900

    [#5] view 완료

---
+++ src/main/webapp/resources/js/like.js
@@ -0,0 +1,23 @@
+$(document).ready(function (){
+    $('#like_heart').on('click', function (e){
+        $('.like_white').toggleClass('green');
+    });
+});
+
+$(document).ready(function (){
+    $('#artist_heart1').on('click', function (e){
+        $('.artist_white1').toggleClass('artist_green1');
+    });
+});
+
+$(document).ready(function (){
+    $('#artist_heart2').on('click', function (e){
+        $('.artist_white2').toggleClass('artist_green2');
+    });
+});
+
+$(document).ready(function (){
+    $('#artist_heart3').on('click', function (e){
+        $('.artist_white3').toggleClass('artist_green3');
+    });
+});
